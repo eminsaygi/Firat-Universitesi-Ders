f2b38d68d4c168be1efd5b2bda411a206422ea03
Emin Saygı <emin_0001@hotmail.com>
2022-11-19 20:01:00 +0300

    Yeni dokümanlar eklendi.

---
+++ 2.1/YMH217 Nesne Tabanlı Programlama/Çıkmış Sorular/NesneTabanli Cikmis Sorulari 2020/Final/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Merhaba Dunya");
+            sb.Insert(6, "iyi");
+            Console.WriteLine(sb);
+
+        }
+    }
+
+    /*
+      class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    */
+
+
+
+}
